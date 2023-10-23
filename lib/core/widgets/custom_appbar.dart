cc8a0073ebc8dd672fdb208ea0d0c36b08e67923
Ali Abdelnaby <aliabdelnabi239@gmail.com>
2023-10-23 21:32:46 +0300

    Create payment item

--- lib/core/widgets/custom_appbar.dart
+++ lib/core/widgets/custom_appbar.dart
@@ -3,7 +3,7 @@ import 'package:flutter_paymob/core/utils/styles.dart';
 
 AppBar buildAppBar({required final String title}) {
   return AppBar(
-    leading: const Icon(Icons.arrow_back, size: 30),
+    // leading: const Icon(Icons.arrow_back, size: 30),
     centerTitle: true,
     title: Text(
       title,
--- lib/features/checkout/presentation/views/payment_details.dart
+++ lib/features/checkout/presentation/views/payment_details.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_paymob/core/widgets/custom_appbar.dart';
+import 'package:flutter_paymob/features/checkout/presentation/widgets/payment_details_view_body.dart';
 
 class PaymentDetailsView extends StatelessWidget {
   const PaymentDetailsView({super.key});
@@ -8,6 +9,7 @@ class PaymentDetailsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(title: 'Payment Details'),
+      body: const PaymentDetailsViewBody(),
     );
   }
 }
