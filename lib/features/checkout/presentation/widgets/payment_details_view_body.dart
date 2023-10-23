d4237e9dd350bf2fad8485580451d77804f61aca
Ali Abdelnaby <aliabdelnabi239@gmail.com>
2023-10-23 23:46:06 +0300

    validate user input

--- lib/features/checkout/presentation/widgets/payment_details_view_body.dart
+++ lib/features/checkout/presentation/widgets/payment_details_view_body.dart
@@ -1,25 +1,48 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_paymob/core/widgets/custom_btn.dart';
 import 'package:flutter_paymob/features/checkout/presentation/widgets/custom_credit_card.dart';
 import 'package:flutter_paymob/features/checkout/presentation/widgets/payment_methods_list_view.dart';
 
-class PaymentDetailsViewBody extends StatelessWidget {
+class PaymentDetailsViewBody extends StatefulWidget {
   const PaymentDetailsViewBody({super.key});
 
   @override
+  State<PaymentDetailsViewBody> createState() => _PaymentDetailsViewBodyState();
+}
+
+class _PaymentDetailsViewBodyState extends State<PaymentDetailsViewBody> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+
+  @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(
+    return CustomScrollView(
       slivers: [
-        SliverToBoxAdapter(child: PaymentMethodsListView()),
-        SliverToBoxAdapter(child: CustomCreditCard()),
+        const SliverToBoxAdapter(child: PaymentMethodsListView()),
+        SliverToBoxAdapter(
+            child: CustomCreditCard(
+          formKey: formKey,
+          autovalidateMode: autovalidateMode,
+        )),
         SliverFillRemaining(
           hasScrollBody: false,
           child: Align(
             alignment: Alignment.bottomCenter,
             child: Padding(
-              padding: EdgeInsets.only(bottom: 12.0, left: 16, right: 16),
+              padding: const EdgeInsets.only(bottom: 12.0, left: 16, right: 16),
               child: CustomButton(
                 text: "Pay",
+                onTap: () {
+                  if (formKey.currentState!.validate()) {
+                    formKey.currentState!.save();
+                    log("pay");
+                  } else {
+                    autovalidateMode = AutovalidateMode.always;
+                    setState(() {});
+                  }
+                },
               ),
             ),
           ),
