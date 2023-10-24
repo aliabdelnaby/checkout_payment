03682518b59a1e8e65606ff4af040781069e16de
Ali Abdelnaby <aliabdelnabi239@gmail.com>
2023-10-24 20:21:40 +0300

    Refactor code

--- lib/features/checkout/presentation/widgets/thank_you_view_body.dart
+++ lib/features/checkout/presentation/widgets/thank_you_view_body.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_paymob/features/checkout/presentation/widgets/custom_check_icon.dart';
+import 'package:flutter_paymob/features/checkout/presentation/widgets/custom_dashed_line.dart';
+import 'package:flutter_paymob/features/checkout/presentation/widgets/thank_you_card.dart';
 
 class ThankYouViewBody extends StatelessWidget {
   const ThankYouViewBody({super.key});
@@ -10,32 +13,12 @@ class ThankYouViewBody extends StatelessWidget {
       child: Stack(
         clipBehavior: Clip.none,
         children: [
-          Container(
-            decoration: ShapeDecoration(
-              color: const Color(0xFFD9D9D9),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-            ),
-          ),
+          const ThankYouCard(),
           Positioned(
             bottom: MediaQuery.sizeOf(context).height * .2 + 20,
             left: 28,
             right: 28,
-            child: Row(
-              children: List.generate(
-                30,
-                (index) => Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 2),
-                    child: Container(
-                      height: 2,
-                      color: const Color(0xFFB7B7B7),
-                    ),
-                  ),
-                ),
-              ),
-            ),
+            child: const CustomDashedLine(),
           ),
           Positioned(
             left: -20,
@@ -55,19 +38,7 @@ class ThankYouViewBody extends StatelessWidget {
             left: 0,
             right: 0,
             top: -50,
-            child: CircleAvatar(
-              radius: 50,
-              backgroundColor: Color(0xFFD9D9D9),
-              child: CircleAvatar(
-                radius: 40,
-                backgroundColor: Color(0xff34A853),
-                child: Icon(
-                  Icons.check,
-                  color: Colors.white,
-                  size: 50,
-                ),
-              ),
-            ),
+            child: CustomCheckIcon(),
           ),
         ],
       ),
