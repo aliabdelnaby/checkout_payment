e0e1e01ca869fb3b56404d3158157ee85250f534
Ali Abdelnaby <aliabdelnabi239@gmail.com>
2023-10-25 17:45:24 +0300

    Create api service

--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -1,4 +1,11 @@
-class StripeService
-{
+import 'package:flutter_paymob/features/checkout/data/models/payment_intent_input_model.dart';
+import 'package:flutter_paymob/features/checkout/data/models/payment_intent_model/payment_intent_model.dart';
 
+class StripeService {
+
+  // PaymentIntentModel createPaymentIntent(
+  //     PaymentIntentInputModel paymentIntentInputModel) 
+  //     {
+
+  //     }
 }
