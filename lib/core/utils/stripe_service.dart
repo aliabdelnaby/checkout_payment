4b8e6fdc913eb991b48692ec3e60663dd335e54f
Ali Abdelnaby <aliabdelnabi239@gmail.com>
2023-10-28 16:51:25 +0200

    Implement payment intent

--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -1,11 +1,19 @@
+import 'package:flutter_paymob/core/utils/api_keys.dart';
+import 'package:flutter_paymob/core/utils/api_service.dart';
 import 'package:flutter_paymob/features/checkout/data/models/payment_intent_input_model.dart';
 import 'package:flutter_paymob/features/checkout/data/models/payment_intent_model/payment_intent_model.dart';
 
 class StripeService {
-
-  // PaymentIntentModel createPaymentIntent(
-  //     PaymentIntentInputModel paymentIntentInputModel) 
-  //     {
-
-  //     }
+  final String secretKey = '';
+  final ApiService apiService = ApiService();
+  Future<PaymentIntentModel> createPaymentIntent(
+      PaymentIntentInputModel paymentIntentInputModel) async {
+    var response = await apiService.post(
+      body: paymentIntentInputModel.toJson(),
+      url: "https://api.stripe.com/v1/payment_intents",
+      token: ApiKeys.secretKey,
+    );
+    var paymentIntentModel = PaymentIntentModel.fromJson(response.data);
+    return paymentIntentModel;
+  }
 }
